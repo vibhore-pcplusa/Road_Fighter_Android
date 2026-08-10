fa3738c3343018106e5de2f6fb0678a7a1739d37
vibhore-pcplusa <vibhore@pcplusa.com>
2026-08-10 23:24:38 +0530

    andorid permission ad_id is added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.vibhorejain.road_fighter"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         // 🏆 PASTE THIS LINE HERE:
