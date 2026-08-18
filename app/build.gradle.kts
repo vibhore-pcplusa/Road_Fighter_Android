6bcea4721d5967351c3e21696e8ae1708c8ab8bd
vibhore-pcplusa <vibhore@pcplusa.com>
2026-08-18 16:42:37 +0530

    Bump versionCode to 7 and versionName to 7.0 for adaptive ad and sound toggle release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.vibhorejain.road_fighter"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "6.0"
+        versionCode = 7
+        versionName = "7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         // 🏆 PASTE THIS LINE HERE:
