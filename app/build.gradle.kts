de02e491454689a2c50489f5f351ed395ba5acc7
vibhore-pcplusa <vibhore@pcplusa.com>
2026-08-18 17:19:27 +0530

    Update native debug symbol level to FULL and bump version to 11.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.vibhorejain.road_fighter"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "10.0"
+        versionCode = 11
+        versionName = "11.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         // 🏆 PASTE THIS LINE HERE:
@@ -32,7 +32,7 @@ android {
                 "proguard-rules.pro"
             )
             ndk {
-                debugSymbolLevel = "full"
+                debugSymbolLevel = "FULL"
             }
         }
     }
