e761ea125c4a921bec2b4217d5e23e1047745b32
vibhore-pcplusa <vibhore@pcplusa.com>
2026-08-31 18:13:22 +0530

    Bump versionCode to 22 and versionName to 22.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vibhorejain.road_fighter"
         minSdk = 24
         targetSdk = 36
-        versionCode = 21
-        versionName = "21.0"
+        versionCode = 22
+        versionName = "22.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         // 🏆 PASTE THIS LINE HERE:
